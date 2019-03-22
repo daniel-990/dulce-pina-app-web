2075d556a0feb6ef96c90c39fa64d38b3bf20694
Daniel Arango <danielarango990@gmail.com>
2019-03-22 13:13:18 -0500

    carpera build y demas
    carpeta build y demas componentes nuevos de la app, archivo database y .next

---
+++ database/airtableconnections.js
@@ -0,0 +1,14 @@
+/**
+ *  Datos de coneccion a la Api de Air Table
+*/
+
+export const moduleAirtableData = {
+    "config": 
+    {
+        "apiKey": "keyFEarKdErHe2bsE",
+        "url": "https://api.airtable.com/v0/app15HzGUrB5V6lSu/Imported%20table?api_key="
+    },
+    mostrarInformacion(){
+      return this;
+    }
+}
